43656fd1d3f8a4517ceab31201580e6f8c460d46
siraajul <shirajulislamparvez@gmail.com>
2025-01-01 10:57:55 +0600

    Update Projects

--- Module_01/Live_Class_03/tailwind.config.js
+++ Module_01/Live_Class_03/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
